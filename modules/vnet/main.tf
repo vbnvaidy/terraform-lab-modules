b1b96b7755bb27597a8e18b0ec3a72bc3546ca57
vbnvaidy <vbnvaidy@gmail.com>
2024-06-14 06:43:49 +0000

    remove reference to resource group

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -10,8 +10,8 @@ resource "azurerm_virtual_network" "vnet" {
 }
 resource "azurerm_subnet" "web_subnet" {
   name                 = "${local.resource_name_prefix}-${var.web_subnet_name}"
-  resource_group_name  = azurerm_resource_group.myrg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
+  resource_group_name  = var.resource_group_location 
+  virtual_network_name = var.resource_group_name 
   address_prefixes     = var.web_subnet_address
 }
 
