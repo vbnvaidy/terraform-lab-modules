39fbd82f85f59b0ff90618e5d7394f5545fe90e0
vbnvaidy <vbnvaidy@gmail.com>
2024-06-14 06:16:07 +0000

    remove resource group creation

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,10 +1,3 @@
-#how to create a resource group. 
-resource "azurerm_resource_group" "myrg" { #this is the refrence it will be managed by terraform to store your logs
-  name = "${local.resource_name_prefix}-${var.resource_group_name}"
-  #sap-dev-rg-default
-  location = var.resource_group_location
-  tags     = local.common_tags
-}
 resource "azurerm_virtual_network" "vnet" {
   name = "${local.resource_name_prefix}-${var.vnet_name}"
   #this vnet need to be part of your resource group and resource group location
