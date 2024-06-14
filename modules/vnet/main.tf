d87cd9023766c7a2979a0de9bb1329f666221e86
vbnvaidy <54671953+vbnvaidy@users.noreply.github.com>
2024-06-14 12:02:13 +0530

    Update main.tf
    edit variable assignment in vnet, rg removed

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
   name = "${local.resource_name_prefix}-${var.vnet_name}"
   #this vnet need to be part of your resource group and resource group location
-  location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group.myrg.name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
   address_space       = var.vnet_address_space
 
   tags = local.common_tags
